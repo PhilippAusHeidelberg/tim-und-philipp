55a385121ba5fcea247657786fa6365267142f13
PhilippAusHeidelberg <110686148+PhilippAusHeidelberg@users.noreply.github.com>
2022-08-29 19:51:36 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,78 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+        basic.pause(1)
+        basic.showLeds(`
+            . # # # .
+            . # . # .
+            . # # # .
+            . # . . .
+            . # . . .
+            `)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # # # # #
+            # # . # #
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.pause(1)
+        basic.showLeds(`
+            # # # # #
+            # # . # #
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . # .
+            . . . # .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.pause(1)
+        basic.showLeds(`
+            . . . # .
+            . . . # .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
